6356cd8653a4b74f57f99e6e25f8e68dd62d69e3
Vitor Adonai <infinitanet@gmail.com>
2020-10-13 16:17:40 -0300

    Meu primeiro componente Arrow funcion

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function App () {
-    return (
-        <View>
-            <Text>Meu primeiro componente!</Text>
-        </View>
-    )
+/*
+// Função normal
+export default function () {
+    return <View><Text>Meu primeiro componente!</Text></View>;
+}
+// Arrow funcion
+export default () => {
+    return <View><Text>Meu primeiro componente!</Text></View>;
+}
+// Arrow function minimalista
+export default () => <View><Text>Meu primeiro componente!</Text></View>;
+*/
+
+export default function () {
+    return <View><Text>Meu primeiro componente!</Text></View>;
 }
