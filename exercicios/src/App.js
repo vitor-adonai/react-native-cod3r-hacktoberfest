954776bfa4c33ba063f612a3107cecb85c015e9a
Vitor Adonai <infinitanet@gmail.com>
2020-10-13 16:52:48 -0300

    Console warn

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -9,7 +9,7 @@ export default function App () {
         <View style={style.App}>
             {/*<Primeiro></Primeiro> */}
             {/* <Multi></Multi> */}
-            {/* <Primeiro></Primeiro> */}
+            <Primeiro></Primeiro>
         </View>
     )
 }
--- exercicios/src/components/Primeiro.js
+++ exercicios/src/components/Primeiro.js
@@ -3,6 +3,7 @@ import {View, Text} from 'react-native'
 import Estilo from './estilo'
 
 export default function Primeiro () {
+    console.warn('OPAA!')
     return (
         <View>
             <Text style={Estilo.fontG}>Primeiro</Text>
