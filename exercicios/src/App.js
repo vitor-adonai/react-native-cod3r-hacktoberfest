a571ed3381d8641ba1de1111d51a31e63421da13
Vitor Adonai <infinitanet@gmail.com>
2020-10-13 16:34:20 -0300

    Primeir componente de fato para o curso

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Primeiro from './components/Primeiro'
 
-/*
-// Função normal
-export default function () {
-    return <View><Text>Meu primeiro componente!</Text></View>;
-}
-// Arrow funcion
-export default () => {
-    return <View><Text>Meu primeiro componente!</Text></View>;
-}
-// Arrow function minimalista
-export default () => <View><Text>Meu primeiro componente!</Text></View>;
-*/
-
-export default function () {
-    return <View><Text>Meu primeiro componente!</Text></View>;
+export default function App () {
+    return (
+        <View>
+            <Primeiro></Primeiro>
+            {/* <Primeiro></Primeiro> */}
+        </View>
+    )
 }
